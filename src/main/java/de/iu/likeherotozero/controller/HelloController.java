7cd85618c9dda941bef1ef2348ce6bc1a41bfca4
Marcel <kroencke.marcel@icloud.com>
2025-06-18 19:28:28 +0200

    ✨ Projektgrundgerüst: Spring Boot Setup + HelloController - Initiales Maven-Projekt mit Spring Boot erstellt - HelloController unter /hello als Funktionstest implementiert - Anwendung erfolgreich lokal lauffähig

---
+++ src/main/java/de/iu/likeherotozero/controller/HelloController.java
@@ -0,0 +1,13 @@
+package de.iu.likeherotozero.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, Like Hero To Zero!";
+    }
+}
